8973b3f6ac23098353b0bb0daaff5ff7460e17ae
Dogus Gunes <dogusgunes68@gmail.com>
2023-09-14 20:23:59 +0300

    dist folder deleted

--- dist/config/auth.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt_secret: "sdkoakdasdwl843153154wdasdsadasdasdsad",
-    jwt_expires: 10 * 24 * 60 * 60 * 1000 // 10 days
-};
--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    host: "localhost",
-    port: 5432,
-    databasename: "shopping",
-    username: "root",
-    password: "root",
-};
